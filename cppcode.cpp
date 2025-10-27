c2cfdedba4b4fe6331cb0da69ef7b97a5a9ced11
fernandathays95-blip <fernandathays95@gmail.com>
2025-10-26 21:03:26 -0300

    Update cppcode.cpp

--- cppcode.cpp
+++ cppcode.cpp
@@ -11,7 +11,13 @@ int main() {
 
     // Informações do sistema
     print_system_info();
+    printf("=== Starting VRD GPU Test ===\n");
 
+    vrd_gpu_init(640, 480);
+    vrd_gpu_clear(0x000000); // preto
+    vrd_gpu_draw_pixel(10, 10, 0xFFFFFF); // pixel branco
+    vrd_gpu_info();
+    vrd_gpu_shutdown();
     return 0;
 }
 
